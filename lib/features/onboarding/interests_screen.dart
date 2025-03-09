3d7169b555d71d8cc8ea0a530282a2bb0e3b1241
chunbae-like-talisker <nsy3576@gmail.com>
2025-03-09 16:47:56 +0900

    Temp

--- lib/features/onboarding/interests_screen.dart
+++ lib/features/onboarding/interests_screen.dart
@@ -1,10 +1,166 @@
 import 'package:flutter/material.dart';
+import 'package:tiktok_clone/constants/gaps.dart';
+import 'package:tiktok_clone/constants/sizes.dart';
+import 'package:tiktok_clone/features/onboarding/tutorial_screen.dart';
+import 'package:tiktok_clone/features/onboarding/widgets/interest_button.dart';
 
-class InterestsScreen extends StatelessWidget {
+class InterestsScreen extends StatefulWidget {
   const InterestsScreen({super.key});
+
+  static const interests = [
+    "Daily Life",
+    "Comedy",
+    "Entertainment",
+    "Animals",
+    "Food",
+    "Beauty & Style",
+    "Drama",
+    "Learning",
+    "Talent",
+    "Sports",
+    "Auto",
+    "Family",
+    "Fitness & Health",
+    "DIY & Life Hacks",
+    "Arts & Crafts",
+    "Dance",
+    "Outdoors",
+    "Oddly Satisfying",
+    "Home & Garden",
+    "Daily Life",
+    "Comedy",
+    "Entertainment",
+    "Animals",
+    "Food",
+    "Beauty & Style",
+    "Drama",
+    "Learning",
+    "Talent",
+    "Sports",
+    "Auto",
+    "Family",
+    "Fitness & Health",
+    "DIY & Life Hacks",
+    "Arts & Crafts",
+    "Dance",
+    "Outdoors",
+    "Oddly Satisfying",
+    "Home & Garden",
+  ];
+
+  @override
+  State<InterestsScreen> createState() => _InterestsScreenState();
+}
+
+class _InterestsScreenState extends State<InterestsScreen> {
+  final ScrollController _scrollController = ScrollController();
+
+  bool _showTitle = false;
+
+  void _onScroll() {
+    if (_scrollController.offset > 100) {
+      if (_showTitle) return;
+      setState(() {
+        _showTitle = true;
+      });
+    } else {
+      if (!_showTitle) return;
+      setState(() {
+        _showTitle = false;
+      });
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _scrollController.addListener(_onScroll);
+  }
+
+  @override
+  void dispose() {
+    _scrollController.dispose();
+    super.dispose();
+  }
+
+  void _onNextTap() {
+    Navigator.push(
+      context,
+      MaterialPageRoute(builder: (context) => TutorialScreen()),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: AnimatedOpacity(
+          opacity: _showTitle ? 1 : 0,
+          duration: Duration(milliseconds: 300),
+          child: Text("Choose your interests"),
+        ),
+      ),
+      body: Scrollbar(
+        controller: _scrollController,
+        child: SingleChildScrollView(
+          controller: _scrollController,
+          child: Padding(
+            padding: EdgeInsets.only(
+              left: Sizes.size24,
+              right: Sizes.size24,
+              bottom: Sizes.size16,
+            ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Gaps.v32,
+                Text(
+                  "Choose your interests",
+                  style: TextStyle(
+                    fontSize: Sizes.size40,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                Gaps.v20,
+                Text(
+                  "Get better video recommendation",
+                  style: TextStyle(fontSize: Sizes.size16),
+                ),
+                Gaps.v64,
+                Wrap(
+                  runSpacing: 15,
+                  spacing: 15,
+                  children: [
+                    for (var interest in InterestsScreen.interests)
+                      InterestButton(interest: interest),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+      bottomNavigationBar: BottomAppBar(
+        elevation: 2,
+        padding: EdgeInsets.only(
+          bottom: Sizes.size40,
+          top: Sizes.size16,
+          left: Sizes.size24,
+          right: Sizes.size24,
+        ),
+        child: GestureDetector(
+          onTap: _onNextTap,
+          child: Container(
+            padding: EdgeInsets.symmetric(vertical: Sizes.size20),
+            decoration: BoxDecoration(color: Theme.of(context).primaryColor),
+            child: Text(
+              "Next",
+              textAlign: TextAlign.center,
+              style: TextStyle(color: Colors.white, fontSize: Sizes.size20),
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
---
+++ lib/features/onboarding/tutorial_screen.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:tiktok_clone/constants/gaps.dart';
+import 'package:tiktok_clone/constants/sizes.dart';
+
+class TutorialScreen extends StatefulWidget {
+  const TutorialScreen({super.key});
+
+  @override
+  State<TutorialScreen> createState() => _TutorialScreenState();
+}
+
+class _TutorialScreenState extends State<TutorialScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Padding(
+          padding: EdgeInsets.symmetric(horizontal: Sizes.size24),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Gaps.v52,
+              Text(
+                "Watch cool videos!",
+                style: TextStyle(
+                  fontSize: Sizes.size40,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              Gaps.v16,
+              Text(
+                "Videos are personalized for you based on what you watch, like, and share.",
+                style: TextStyle(fontSize: Sizes.size20),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
