aed277ecf6b5345cb295f9019e87e8cda2dafb41
chunbae-like-talisker <nsy3576@gmail.com>
2025-03-20 21:12:03 +0900

    #7.8 Video UI

---
+++ lib/features/videos/video_timeline_screen.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+import 'package:tiktok_clone/features/videos/widgets/video_post.dart';
+
+class VideoTimelineScreen extends StatefulWidget {
+  const VideoTimelineScreen({super.key});
+
+  @override
+  State<VideoTimelineScreen> createState() => _VideoTimelineScreenState();
+}
+
+class _VideoTimelineScreenState extends State<VideoTimelineScreen> {
+  int _itemCount = 4;
+
+  final PageController _pageController = PageController();
+
+  final Duration _scrollDuration = Duration(milliseconds: 250);
+  final Curve _scrollCurve = Curves.linear;
+
+  void _onPageChanged(int page) {
+    _pageController.animateToPage(
+      page,
+      duration: Duration(milliseconds: 150),
+      curve: Curves.linear,
+    );
+
+    if (page == _itemCount - 1) {
+      _itemCount += 4;
+      setState(() {});
+    }
+  }
+
+  void _onVideoFinished() {
+    return;
+    // _pageController.nextPage(duration: _scrollDuration, curve: _scrollCurve);
+  }
+
+  @override
+  void dispose() {
+    _pageController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return PageView.builder(
+      controller: _pageController,
+      scrollDirection: Axis.vertical,
+      onPageChanged: _onPageChanged,
+      itemCount: _itemCount,
+      itemBuilder:
+          (context, index) =>
+              VideoPost(onVideoFinished: _onVideoFinished, index: index),
+    );
+  }
+}
---
+++ lib/features/videos/widgets/video_post.dart
@@ -0,0 +1,201 @@
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:tiktok_clone/constants/gaps.dart';
+import 'package:tiktok_clone/constants/sizes.dart';
+import 'package:tiktok_clone/features/videos/widgets/video_button.dart';
+import 'package:video_player/video_player.dart';
+import 'package:visibility_detector/visibility_detector.dart';
+
+class VideoPost extends StatefulWidget {
+  const VideoPost({
+    super.key,
+    required this.onVideoFinished,
+    required this.index,
+  });
+
+  final Function onVideoFinished;
+  final int index;
+
+  @override
+  State<VideoPost> createState() => _VideoPostState();
+}
+
+class _VideoPostState extends State<VideoPost>
+    with SingleTickerProviderStateMixin {
+  late final VideoPlayerController _videoPlayerController;
+  late final AnimationController _animationController;
+
+  final Duration _animationDuration = Duration(milliseconds: 200);
+
+  bool _isPaused = false;
+  bool _isShowMore = false;
+
+  void _onVideoChange() {
+    if (_videoPlayerController.value.isInitialized) {
+      if (_videoPlayerController.value.duration ==
+          _videoPlayerController.value.position) {
+        widget.onVideoFinished();
+      }
+    }
+  }
+
+  void _initVideoPlayer() async {
+    _videoPlayerController = VideoPlayerController.asset(
+      "assets/videos/video.mp4",
+    );
+    await _videoPlayerController.initialize();
+    await _videoPlayerController.setLooping(true);
+    setState(() {});
+    _videoPlayerController.addListener(_onVideoChange);
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _initVideoPlayer();
+
+    _animationController = AnimationController(
+      vsync: this,
+      lowerBound: 1.0,
+      upperBound: 1.5,
+      value: 1.5,
+      duration: _animationDuration,
+    );
+  }
+
+  @override
+  void dispose() {
+    _videoPlayerController.dispose();
+    super.dispose();
+  }
+
+  void _onVisibilityChanged(VisibilityInfo info) {
+    if (info.visibleFraction == 1 && !_videoPlayerController.value.isPlaying) {
+      _videoPlayerController.play();
+    }
+  }
+
+  void _togglePause() {
+    if (_videoPlayerController.value.isPlaying) {
+      _videoPlayerController.pause();
+      _animationController.reverse();
+    } else {
+      _videoPlayerController.play();
+      _animationController.forward();
+    }
+    setState(() {
+      _isPaused = !_isPaused;
+    });
+  }
+
+  void _onShowMoreTap() {
+    setState(() {
+      _isShowMore = !_isShowMore;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return VisibilityDetector(
+      key: Key("${widget.index}"),
+      onVisibilityChanged: _onVisibilityChanged,
+      child: Stack(
+        children: [
+          Positioned.fill(
+            child:
+                _videoPlayerController.value.isInitialized
+                    ? VideoPlayer(_videoPlayerController)
+                    : Container(color: Colors.black),
+          ),
+          Positioned.fill(child: GestureDetector(onTap: _togglePause)),
+          Positioned.fill(
+            child: IgnorePointer(
+              child: Center(
+                child: AnimatedBuilder(
+                  animation: _animationController,
+                  builder: (context, child) {
+                    return Transform.scale(
+                      scale: _animationController.value,
+                      child: child,
+                    );
+                  },
+                  child: AnimatedOpacity(
+                    opacity: _isPaused ? 1 : 0,
+                    duration: _animationDuration,
+                    child: FaIcon(
+                      FontAwesomeIcons.play,
+                      color: Colors.white,
+                      size: Sizes.size52,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ),
+          Positioned(
+            bottom: 20,
+            left: 10,
+            right: 70,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  "@니꼬",
+                  style: TextStyle(
+                    fontSize: Sizes.size20,
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                Gaps.v10,
+                Text(
+                  "This is my house in Thailand!!! The surroundings are truly breathtaking! You should come and live here too—it will be an incredibly enjoyable experience!",
+                  style: TextStyle(fontSize: Sizes.size16, color: Colors.white),
+                  overflow:
+                      _isShowMore
+                          ? TextOverflow.visible
+                          : TextOverflow.ellipsis,
+                ),
+                // Gaps.v10,
+                TextButton(
+                  onPressed: _onShowMoreTap,
+                  child: Text(
+                    _isShowMore ? "Hide" : "Show more",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: Sizes.size12,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Positioned(
+            bottom: 20,
+            right: 10,
+            child: Column(
+              children: [
+                CircleAvatar(
+                  radius: 25,
+                  backgroundColor: Colors.black,
+                  foregroundColor: Colors.white,
+                  foregroundImage: NetworkImage(
+                    "https://tamagotchi-official.com/tamagotchi/jp/character/2023/06/03/7bDlVADQACLZJXuR/%E3%81%BF%E3%81%BF%E3%81%A3%E3%81%A1_%E6%9B%B8%E3%81%8D%E5%87%BA%E3%81%97%E6%AD%A3%E6%96%B9%E5%BD%A2.png",
+                  ),
+                  child: Text("니꼬"),
+                ),
+                Gaps.v24,
+                VideoButton(icon: FontAwesomeIcons.solidHeart, text: "2.9M"),
+                Gaps.v24,
+                VideoButton(icon: FontAwesomeIcons.solidComment, text: "33K"),
+                Gaps.v24,
+                VideoButton(icon: FontAwesomeIcons.share, text: "Share"),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
