6f0e27b37f133cb846b7ecd1977ba617ccf0b77d
chunbae-like-talisker <nsy3576@gmail.com>
2025-03-26 22:38:32 +0900

    #12.7 PersistentTabBar

--- lib/features/users/user_profile_screen.dart
+++ lib/features/users/user_profile_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:tiktok_clone/constants/gaps.dart';
 import 'package:tiktok_clone/constants/sizes.dart';
+import 'package:tiktok_clone/features/users/widgets/persistent_tab_bar.dart';
 
 class UserProfileScreen extends StatefulWidget {
   const UserProfileScreen({super.key});
@@ -13,121 +14,275 @@ class UserProfileScreen extends StatefulWidget {
 class _UserProfileScreenState extends State<UserProfileScreen> {
   @override
   Widget build(BuildContext context) {
-    return CustomScrollView(
-      slivers: [
-        SliverAppBar(
-          title: Text("니꼬"),
-          actions: [
-            IconButton(
-              onPressed: () {},
-              icon: FaIcon(FontAwesomeIcons.gear, size: Sizes.size20),
-            ),
-          ],
-        ),
-        SliverToBoxAdapter(
-          child: Column(
-            children: [
-              CircleAvatar(
-                radius: 50,
-                foregroundImage: NetworkImage(
-                  "https://item.kakaocdn.net/do/00bef4bad789c2c98ccd9229d074b9384022de826f725e10df604bf1b9725cfd",
-                ),
-                child: Text("니꼬"),
-              ),
-              Gaps.v20,
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text(
-                    "@니꼬",
-                    style: TextStyle(
-                      fontWeight: FontWeight.w600,
-                      fontSize: Sizes.size18,
-                    ),
-                  ),
-                  Gaps.h5,
-                  FaIcon(
-                    FontAwesomeIcons.solidCircleCheck,
-                    size: Sizes.size16,
-                    color: Colors.blue.shade500,
+    return SafeArea(
+      child: DefaultTabController(
+        length: 2,
+        child: NestedScrollView(
+          headerSliverBuilder: (context, innerBoxIsScrolled) {
+            return [
+              SliverAppBar(
+                title: Text("니꼬"),
+                actions: [
+                  IconButton(
+                    onPressed: () {},
+                    icon: FaIcon(FontAwesomeIcons.gear, size: Sizes.size20),
                   ),
                 ],
               ),
-              Gaps.v24,
-              SizedBox(
-                height: Sizes.size48,
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
+              SliverToBoxAdapter(
+                child: Column(
                   children: [
-                    Column(
+                    CircleAvatar(
+                      radius: 50,
+                      foregroundImage: NetworkImage(
+                        "https://item.kakaocdn.net/do/00bef4bad789c2c98ccd9229d074b9384022de826f725e10df604bf1b9725cfd",
+                      ),
+                      child: Text("니꼬"),
+                    ),
+                    Gaps.v20,
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
                       children: [
                         Text(
-                          "97",
+                          "@니꼬",
                           style: TextStyle(
-                            fontWeight: FontWeight.bold,
+                            fontWeight: FontWeight.w600,
                             fontSize: Sizes.size18,
                           ),
                         ),
-                        Gaps.v3,
-                        Text(
-                          "Following",
-                          style: TextStyle(color: Colors.grey.shade500),
+                        Gaps.h5,
+                        FaIcon(
+                          FontAwesomeIcons.solidCircleCheck,
+                          size: Sizes.size16,
+                          color: Colors.blue.shade500,
                         ),
                       ],
                     ),
-                    VerticalDivider(
-                      width: Sizes.size32,
-                      thickness: Sizes.size1,
-                      color: Colors.grey.shade400,
-                      indent: Sizes.size14,
-                      endIndent: Sizes.size14,
+                    Gaps.v24,
+                    SizedBox(
+                      height: Sizes.size48,
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Column(
+                            children: [
+                              Text(
+                                "97",
+                                style: TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: Sizes.size18,
+                                ),
+                              ),
+                              Gaps.v3,
+                              Text(
+                                "Following",
+                                style: TextStyle(color: Colors.grey.shade500),
+                              ),
+                            ],
+                          ),
+                          VerticalDivider(
+                            width: Sizes.size32,
+                            thickness: Sizes.size1,
+                            color: Colors.grey.shade400,
+                            indent: Sizes.size14,
+                            endIndent: Sizes.size14,
+                          ),
+                          Column(
+                            children: [
+                              Text(
+                                "10M",
+                                style: TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: Sizes.size18,
+                                ),
+                              ),
+                              Gaps.v3,
+                              Text(
+                                "Followers",
+                                style: TextStyle(color: Colors.grey.shade500),
+                              ),
+                            ],
+                          ),
+                          VerticalDivider(
+                            width: Sizes.size32,
+                            thickness: Sizes.size1,
+                            color: Colors.grey.shade400,
+                            indent: Sizes.size14,
+                            endIndent: Sizes.size14,
+                          ),
+                          Column(
+                            children: [
+                              Text(
+                                "194.3M",
+                                style: TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: Sizes.size18,
+                                ),
+                              ),
+                              Gaps.v3,
+                              Text(
+                                "Likes",
+                                style: TextStyle(color: Colors.grey.shade500),
+                              ),
+                            ],
+                          ),
+                        ],
+                      ),
                     ),
-                    Column(
+                    Gaps.v14,
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
                       children: [
-                        Text(
-                          "10M",
-                          style: TextStyle(
-                            fontWeight: FontWeight.bold,
-                            fontSize: Sizes.size18,
+                        Container(
+                          alignment: Alignment.center,
+                          height: Sizes.size40,
+                          padding: EdgeInsets.symmetric(
+                            horizontal: Sizes.size40,
+                          ),
+                          decoration: BoxDecoration(
+                            color: Theme.of(context).primaryColor,
+                            borderRadius: BorderRadius.all(
+                              Radius.circular(Sizes.size4),
+                            ),
+                          ),
+                          child: Text(
+                            "Follow",
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontWeight: FontWeight.w600,
+                            ),
+                            textAlign: TextAlign.center,
                           ),
                         ),
-                        Gaps.v3,
-                        Text(
-                          "Followers",
-                          style: TextStyle(color: Colors.grey.shade500),
-                        ),
-                      ],
-                    ),
-                    VerticalDivider(
-                      width: Sizes.size32,
-                      thickness: Sizes.size1,
-                      color: Colors.grey.shade400,
-                      indent: Sizes.size14,
-                      endIndent: Sizes.size14,
-                    ),
-                    Column(
-                      children: [
-                        Text(
-                          "194.3M",
-                          style: TextStyle(
-                            fontWeight: FontWeight.bold,
-                            fontSize: Sizes.size18,
+                        Gaps.h5,
+                        Container(
+                          alignment: Alignment.center,
+                          width: Sizes.size40,
+                          height: Sizes.size40,
+                          decoration: BoxDecoration(
+                            border: Border.all(color: Colors.grey.shade300),
+                            borderRadius: BorderRadius.all(
+                              Radius.circular(Sizes.size4),
+                            ),
+                          ),
+                          child: FaIcon(
+                            FontAwesomeIcons.youtube,
+                            size: Sizes.size18,
                           ),
                         ),
-                        Gaps.v3,
-                        Text(
-                          "Likes",
-                          style: TextStyle(color: Colors.grey.shade500),
+                        Gaps.h5,
+                        Container(
+                          alignment: Alignment.center,
+                          width: Sizes.size40,
+                          height: Sizes.size40,
+                          decoration: BoxDecoration(
+                            border: Border.all(color: Colors.grey.shade300),
+                            borderRadius: BorderRadius.all(
+                              Radius.circular(Sizes.size4),
+                            ),
+                          ),
+                          child: FaIcon(
+                            FontAwesomeIcons.caretDown,
+                            size: Sizes.size14,
+                          ),
                         ),
                       ],
                     ),
+                    Gaps.v14,
+                    Padding(
+                      padding: EdgeInsets.symmetric(horizontal: Sizes.size32),
+                      child: Text(
+                        "All hightlights and where to watch live mathces on FIFA+ I wonder how it would loook",
+                        textAlign: TextAlign.center,
+                      ),
+                    ),
+                    Gaps.v14,
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        FaIcon(FontAwesomeIcons.link, size: Sizes.size12),
+                        Gaps.h4,
+                        Text(
+                          "https://nomadcoders.co",
+                          style: TextStyle(fontWeight: FontWeight.w600),
+                        ),
+                      ],
+                    ),
+                    Gaps.v20,
                   ],
                 ),
               ),
-            ],
+              SliverPersistentHeader(
+                delegate: PersistentTabBar(),
+                pinned: true,
+              ),
+            ];
+          },
+          body: SizedBox(
+            width: MediaQuery.of(context).size.width,
+            height: MediaQuery.of(context).size.height,
+            child: TabBarView(
+              children: [
+                GridView.builder(
+                  keyboardDismissBehavior:
+                      ScrollViewKeyboardDismissBehavior.onDrag,
+                  itemCount: 20,
+                  padding: EdgeInsets.zero,
+                  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 3,
+                    crossAxisSpacing: Sizes.size2,
+                    mainAxisSpacing: Sizes.size2,
+                    childAspectRatio: 9 / 14,
+                  ),
+                  itemBuilder:
+                      (context, index) => Column(
+                        children: [
+                          AspectRatio(
+                            aspectRatio: 9 / 14,
+                            child: Stack(
+                              children: [
+                                Positioned.fill(
+                                  child: FadeInImage.assetNetwork(
+                                    fit: BoxFit.cover,
+                                    placeholder:
+                                        "assets/images/placeholder.jpg",
+                                    image:
+                                        "https://pbs.twimg.com/media/GI8tHk-bsAAXvTK?format=jpg&name=large",
+                                  ),
+                                ),
+                                Positioned(
+                                  bottom: Sizes.size4,
+                                  left: Sizes.size4,
+                                  child: Row(
+                                    children: [
+                                      FaIcon(
+                                        FontAwesomeIcons.play,
+                                        size: Sizes.size14,
+                                        color: Colors.white,
+                                      ),
+                                      Gaps.h5,
+                                      Text(
+                                        "4.1M",
+                                        style: TextStyle(
+                                          color: Colors.white,
+                                          fontSize: Sizes.size12,
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ],
+                      ),
+                ),
+                Center(child: Text("Page two")),
+              ],
+            ),
           ),
         ),
-      ],
+      ),
     );
   }
 }
