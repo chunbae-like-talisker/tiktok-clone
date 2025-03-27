f369a8dbdd018a275137784de71e83b84b012952
chunbae-like-talisker <nsy3576@gmail.com>
2025-03-27 11:09:09 +0900

    #13.6 Conclusions

--- lib/features/settings/settings_screen.dart
+++ lib/features/settings/settings_screen.dart
@@ -1,8 +1,23 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
-class SettingsScreen extends StatelessWidget {
+class SettingsScreen extends StatefulWidget {
   const SettingsScreen({super.key});
+
+  @override
+  State<SettingsScreen> createState() => _SettingsScreenState();
+}
+
+class _SettingsScreenState extends State<SettingsScreen> {
+  bool _notifications = false;
+
+  void _onNotificationsChanged(bool? newValue) {
+    if (newValue == null) return;
+    setState(() {
+      _notifications = newValue;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -10,6 +25,18 @@ class SettingsScreen extends StatelessWidget {
       appBar: AppBar(title: Text("Settings")),
       body: ListView(
         children: [
+          SwitchListTile.adaptive(
+            value: _notifications,
+            onChanged: _onNotificationsChanged,
+            title: Text("Enable notifications"),
+            subtitle: Text("Enable notifications"),
+          ),
+          CheckboxListTile(
+            activeColor: Colors.black,
+            value: _notifications,
+            onChanged: _onNotificationsChanged,
+            title: Text("Enable notifications"),
+          ),
           ListTile(
             onTap: () async {
               final date = await showDatePicker(
@@ -43,6 +70,82 @@ class SettingsScreen extends StatelessWidget {
               print(booking);
             },
             title: Text("What is your birthday?"),
+          ),
+          ListTile(
+            title: Text("Log out (iOS)"),
+            textColor: Colors.red,
+            onTap: () {
+              showCupertinoDialog(
+                context: context,
+                builder:
+                    (context) => CupertinoAlertDialog(
+                      title: Text("Are you sure?"),
+                      content: Text("Plx dont go"),
+                      actions: [
+                        CupertinoDialogAction(
+                          onPressed: () => Navigator.of(context).pop(),
+                          child: Text("No"),
+                        ),
+                        CupertinoDialogAction(
+                          onPressed: () => Navigator.of(context).pop(),
+                          isDestructiveAction: true,
+                          child: Text("Yes"),
+                        ),
+                      ],
+                    ),
+              );
+            },
+          ),
+          ListTile(
+            title: Text("Log out (Android)"),
+            textColor: Colors.red,
+            onTap: () {
+              showDialog(
+                context: context,
+                builder:
+                    (context) => AlertDialog(
+                      icon: FaIcon(FontAwesomeIcons.skull),
+                      title: Text("Are you sure?"),
+                      content: Text("Plx dont go"),
+                      actions: [
+                        IconButton(
+                          onPressed: () => Navigator.of(context).pop(),
+                          icon: FaIcon(FontAwesomeIcons.car),
+                        ),
+                        TextButton(
+                          onPressed: () => Navigator.of(context).pop(),
+                          child: Text("Yes"),
+                        ),
+                      ],
+                    ),
+              );
+            },
+          ),
+          ListTile(
+            title: Text("Log out (iOS / Bottom)"),
+            textColor: Colors.red,
+            onTap: () {
+              showCupertinoModalPopup(
+                context: context,
+                builder:
+                    (context) => CupertinoActionSheet(
+                      title: Text("Are you sure?"),
+                      message: Text("Please dooooont goooooo"),
+                      actions: [
+                        CupertinoActionSheetAction(
+                          isDefaultAction: true,
+                          onPressed: () => Navigator.of(context).pop(),
+                          child: Text("Not log out"),
+                        ),
+                        CupertinoActionSheetAction(
+                          isDestructiveAction: true,
+                          onPressed: () => Navigator.of(context).pop(),
+                          child: Text("Yes plz."),
+                        ),
+                      ],
+                    ),
+              );
+            },
           ),
           AboutListTile(),
         ],
